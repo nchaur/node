ddc0fcca8e5ffd5ed9a8d741dd717e7651be7907
Laura Chaur <nchaur@gmail.com>
2022-06-15 09:59:49 +0200

    hi

---
+++ app.js
@@ -0,0 +1,20 @@
+const rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+
+function solveRect(l, w) {
+  console.log(`${l} hihihihi, ${w}`);
+
+  if (l <= 0 || w <= 0) {
+    console.log(`must be higher than 0 ${w}, ${l}`);
+  } else {
+    console.log(`first hi ${rect.area(l, w)}`);
+    console.log(rect.perimeter(l, w));
+  }
+}
+
+solveRect(3, 4);
+solveRect(2, 8);
+solveRect(8, 0);
+solveRect(-2, 8);
